466a59e34b66a2333fa8efdd8d377dd4ecd9d9bc
maria <mariacodes77@gmail.com>
2026-01-07 00:17:23 +0300

    chore: init spring boot application

---
+++ src/main/java/com/spacebot/spaceeventsbot/SpaceEventsBotApplication.java
@@ -0,0 +1,13 @@
+package com.spacebot.spaceeventsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceEventsBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceEventsBotApplication.class, args);
+    }
+
+}
