8846d79656860f7e483b2895cf37292b4565d890
maria <mariacodes77@gmail.com>
2026-01-07 17:50:15 +0300

    fix(config): explicitly enable component scan for telegram starter

--- src/main/java/com/spacebot/SpaceEventsBotApplication.java
+++ src/main/java/com/spacebot/SpaceEventsBotApplication.java
@@ -1,12 +1,17 @@
-package com.spacebot.spaceeventsbot;
+package com.spacebot;
 
 import com.spacebot.config.TelegramBotProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigurationProperties(TelegramBotProperties.class)
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.spacebot",
+        "org.telegram.telegrambots"
+})
 public class SpaceEventsBotApplication {
 
     public static void main(String[] args) {
