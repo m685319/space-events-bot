1738fadd07c11ab4a0ef88bdd0bfadcdbf91e737
maria <mariacodes77@gmail.com>
2026-01-09 01:44:11 +0300

    feat(apod): add action for astronomy picture of the day

---
+++ src/main/java/com/spacebot/bot/action/ApodAction.java
@@ -0,0 +1,47 @@
+package com.spacebot.bot.action;
+
+import com.spacebot.dto.apod.ApodResponseDTO;
+import com.spacebot.service.ApodService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@RequiredArgsConstructor
+@Component
+public class ApodAction extends AbstractAction {
+
+    private final ApodService service;
+
+    @Override
+    protected String action() {
+        return CallbackAction.APOD.value();
+    }
+
+    @Override
+    protected BotApiMethod<?> doHandle(Update update) {
+        ApodResponseDTO apod = service.getTodayApod();
+
+        String chatId = update.getCallbackQuery()
+                .getMessage()
+                .getChatId()
+                .toString();
+
+        String text = """
+                🪐 %s
+                📅 %s
+
+                %s
+
+                🔗 %s
+                """.formatted(
+                apod.getTitle(),
+                apod.getDate(),
+                apod.getExplanation(),
+                apod.getUrl()
+        );
+
+        return new SendMessage(chatId, text);
+    }
+}
