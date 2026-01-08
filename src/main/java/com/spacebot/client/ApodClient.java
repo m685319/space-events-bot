ac79e8ebcff4f3f72055b3f2389c51f2c0c3a7ef
maria <mariacodes77@gmail.com>
2026-01-09 01:05:40 +0300

    feat(apod): add client for apod api

---
+++ src/main/java/com/spacebot/client/ApodClient.java
@@ -0,0 +1,25 @@
+package com.spacebot.client;
+
+import com.spacebot.dto.apod.ApodResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@RequiredArgsConstructor
+@Component
+public class ApodClient {
+
+    private final RestClient restClient;
+
+    @Value("${apod.url}")
+    private String apodUrl;
+
+    public ApodResponseDTO getTodayApod() {
+        return restClient.get()
+                .uri(apodUrl)
+                .retrieve()
+                .body(ApodResponseDTO.class);
+    }
+}
+
