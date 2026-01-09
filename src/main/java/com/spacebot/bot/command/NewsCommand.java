7e76278343fcf5d4f33f7de0428c81333f232cac
maria <mariacodes77@gmail.com>
2026-01-09 04:48:18 +0300

    feat(news): add /news command

---
+++ src/main/java/com/spacebot/bot/command/NewsCommand.java
@@ -0,0 +1,31 @@
+package com.spacebot.bot.command;
+
+import com.spacebot.service.NewsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@RequiredArgsConstructor
+@Component
+public class NewsCommand extends AbstractCommand {
+
+    private final NewsService service;
+
+    @Override
+    protected String command() {
+        return "news";
+    }
+
+    @Override
+    protected BotApiMethod<?> doHandle(Update update) {
+        String chatId = update.getMessage()
+                .getChatId()
+                .toString();
+
+        String text = service.getLatestNews();
+        return new SendMessage(chatId, text);
+    }
+}
+
