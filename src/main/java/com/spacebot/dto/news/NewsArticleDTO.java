c8efbb99059216ba925e08fcfdcedc4bbaa19535
maria <mariacodes77@gmail.com>
2026-01-09 03:01:46 +0300

    feat(news): add dto for spaceflight news api

---
+++ src/main/java/com/spacebot/dto/news/NewsArticleDTO.java
@@ -0,0 +1,22 @@
+package com.spacebot.dto.news;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class NewsArticleDTO {
+
+    private String title;
+
+    private String summary;
+
+    private String url;
+
+    @JsonProperty("published_at")
+    private String publishedAt;
+
+    @JsonProperty("news_site")
+    private String newsSite;
+}
---
+++ src/main/java/com/spacebot/dto/news/NewsResponseDTO.java
@@ -0,0 +1,15 @@
+package com.spacebot.dto.news;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class NewsResponseDTO {
+
+    private Integer count;
+
+    private List<NewsArticleDTO> results;
+}
