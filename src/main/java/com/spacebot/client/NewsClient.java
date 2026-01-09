f113220d2c4176aa4d05569031425c9fe8e74dae
Abhishek Verma <abhishekvermaa10@gmail.com>
2026-01-09 12:01:04 +0530

    fix(client): NewsClient will now use newsUrl from properties as a standard

--- src/main/java/com/spacebot/client/NewsClient.java
+++ src/main/java/com/spacebot/client/NewsClient.java
@@ -2,6 +2,7 @@ package com.spacebot.client;
 
 import com.spacebot.dto.news.NewsResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -10,17 +11,17 @@ import org.springframework.web.client.RestClient;
 public class NewsClient {
 
     private final RestClient restClient;
+    @Value("${news.url}")
+    private String newsUrl;
 
     public NewsResponseDTO getLatestNews() {
         return restClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .scheme("https")
-                        .host("api.spaceflightnewsapi.net")
-                        .path("/v4/articles/")
+                .uri(newsUrl, uriBuilder -> uriBuilder
                         .queryParam("limit", 5)
                         .queryParam("ordering", "-published_at")
                         .build())
                 .retrieve()
                 .body(NewsResponseDTO.class);
     }
+
 }
